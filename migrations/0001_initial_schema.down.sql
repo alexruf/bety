b9ed2f96334edd4db8d64f53e42f97e504e0bd1c
Alexander Ruf <alexander@alexruf.net>
2021-02-28 22:23:20 +0100

    Define a entity model and initial database schema
    Even though the data model is far from complete and perfect, it should be enough to get started.

--- migrations/0001_initial_schema.down.sql
+++ migrations/0001_initial_schema.down.sql
@@ -1,1 +1,9 @@
-drop table data;
+drop table if exists bet;
+
+drop table if exists betting_round;
+
+drop table if exists player;
+
+drop table if exists game;
+
+drop table if exists user;
--- migrations/0001_initial_schema.up.sql
+++ migrations/0001_initial_schema.up.sql
@@ -1,6 +1,99 @@
-create table data
+create table game
 (
-    id      TEXT not null
-        primary key,
-    content TEXT
+    id   text not null
+        constraint game_pk
+            primary key,
+    name text not null,
+    type text not null,
+    data blob
 );
+
+create table betting_round
+(
+    id         text not null
+        constraint betting_round_pk
+            primary key,
+    game_id    text not null
+        references game,
+    start_time TEXT not null,
+    end_time   text not null,
+    data       blob
+);
+
+create
+unique index betting_round_id_uindex
+    on betting_round (id);
+
+create
+unique index game_id_uindex
+    on game (id);
+
+create
+unique index game_name_uindex
+    on game (name);
+
+create table user
+(
+    id            text not null
+        constraint user_pk
+            primary key,
+    name          text not null,
+    login         text not null,
+    email         text,
+    password      blob not null,
+    password_salt blob not null
+);
+
+create table player
+(
+    id      text not null
+        constraint player_pk
+            primary key,
+    user_id text not null
+        references user,
+    game_id text not null
+        references game,
+    stats   blob
+);
+
+create table bet
+(
+    id               text not null
+        constraint bet_pk
+            primary key,
+    betting_round_id text not null
+        references betting_round,
+    game_id          text not null
+        references game,
+    player_id        text not null
+        references player,
+    data             blob
+);
+
+create
+unique index bet_game_id_betting_round_id_player_id_uindex
+    on bet (game_id, betting_round_id, player_id);
+
+create
+unique index bet_id_uindex
+    on bet (id);
+
+create
+unique index player_id_uindex
+    on player (id);
+
+create
+unique index player_user_id_game_id_uindex
+    on player (user_id, game_id);
+
+create
+unique index user_id_uindex
+    on user (id);
+
+create
+unique index user_login_uindex
+    on user (login);
+
+create
+unique index user_name_uindex
+    on user (name);
